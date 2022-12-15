d05bb9d408ccb15f7056c7682db7a620582b24ec
Matt <mattkennedy416@gmail.com>
2022-12-14 23:08:25 -0700

    finally have a working python editor, that was easy

--- src/pages/DashboardPages/MedicalDashboardPage.tsx
+++ src/pages/DashboardPages/MedicalDashboardPage.tsx
@@ -17,121 +17,151 @@ import { References } from '@app/components/common/References/References';
 import { useResponsive } from '@app/hooks/useResponsive';
 import * as S from './DashboardPage.styles';
 
+import CodeMirror from '@uiw/react-codemirror';
+import {python} from '@codemirror/lang-python';
+
 const MedicalDashboardPage: React.FC = () => {
   const { isTablet, isDesktop } = useResponsive();
 
   const { t } = useTranslation();
 
+
+  const onChange = React.useCallback((value, viewUpdate) => {
+    console.log('value:', value);
+  }, []);
+
+
   const desktopLayout = (
     <Row>
-      <S.LeftSideCol xl={16} xxl={17}>
-        <Row gutter={[30, 30]}>
-          <Col span={24}>
-            <Row gutter={[30, 30]}>
-              <StatisticsCards />
-            </Row>
-          </Col>
+      hello world!
 
-          <Col id="map" span={24}>
-            <MapCard />
-          </Col>
-
-          <Col id="latest-screenings" span={24}>
-            <ScreeningsCard />
-          </Col>
-
-          <Col id="treatment-plan" xl={24}>
-            <TreatmentCard />
-          </Col>
-
-          <Col id="covid" xl={24}>
-            <CovidCard />
-          </Col>
-
-          <Col id="activity" xl={24} xxl={12}>
-            <ActivityCard />
-          </Col>
-
-          <Col id="health" xl={24} xxl={12}>
-            <HealthCard />
-          </Col>
-
-          <Col id="favorite-doctors" xl={24}>
-            <FavoritesDoctorsCard />
-          </Col>
-
-          <Col id="news" span={24}>
-            <NewsCard />
-          </Col>
-        </Row>
-        <References />
-      </S.LeftSideCol>
-
-      <S.RightSideCol xl={8} xxl={7}>
-        <div id="blood-screening">
-          <BloodScreeningCard />
-        </div>
-        <S.Space />
-        <S.ScrollWrapper id="patient-timeline">
-          <PatientResultsCard />
-        </S.ScrollWrapper>
-      </S.RightSideCol>
+      <CodeMirror
+      value="console.log('hello world!');"
+      height="200px"
+      extensions={[python()]}
+      onChange={onChange}
+    />
     </Row>
+
+
+  //   <Row>
+  //     <S.LeftSideCol xl={16} xxl={17}>
+  //       <Row gutter={[30, 30]}>
+  //         <Col span={24}>
+  //           <Row gutter={[30, 30]}>
+  //             <StatisticsCards />
+  //           </Row>
+  //         </Col>
+
+  //         <Col id="map" span={24}>
+  //           <MapCard />
+  //         </Col>
+
+  //         <Col id="latest-screenings" span={24}>
+  //           <ScreeningsCard />
+  //         </Col>
+
+  //         <Col id="treatment-plan" xl={24}>
+  //           <TreatmentCard />
+  //         </Col>
+
+  //         <Col id="covid" xl={24}>
+  //           <CovidCard />
+  //         </Col>
+
+  //         <Col id="activity" xl={24} xxl={12}>
+  //           <ActivityCard />
+  //         </Col>
+
+  //         <Col id="health" xl={24} xxl={12}>
+  //           <HealthCard />
+  //         </Col>
+
+  //         <Col id="favorite-doctors" xl={24}>
+  //           <FavoritesDoctorsCard />
+  //         </Col>
+
+  //         <Col id="news" span={24}>
+  //           <NewsCard />
+  //         </Col>
+  //       </Row>
+  //       <References />
+  //     </S.LeftSideCol>
+
+  //     <S.RightSideCol xl={8} xxl={7}>
+  //       <div id="blood-screening">
+  //         <BloodScreeningCard />
+  //       </div>
+  //       <S.Space />
+  //       <S.ScrollWrapper id="patient-timeline">
+  //         <PatientResultsCard />
+  //       </S.ScrollWrapper>
+  //     </S.RightSideCol>
+  //   </Row>
   );
+
 
   const mobileAndTabletLayout = (
-    <Row gutter={[20, 20]}>
-      <StatisticsCards />
-
-      {isTablet && (
-        <Col id="map" md={24} order={4}>
-          <MapCard />
-        </Col>
-      )}
-
-      <Col id="latest-screenings" xs={24} md={12} order={(isTablet && 5) || 0}>
-        <ScreeningsCard />
-      </Col>
-
-      <Col id="activity" xs={24} md={12} order={(isTablet && 8) || 0}>
-        <ActivityCard />
-      </Col>
-
-      <Col id="treatment-plan" xs={24} md={24} order={(isTablet && 10) || 0}>
-        <TreatmentCard />
-      </Col>
-
-      <Col id="health" xs={24} md={12} order={(isTablet && 9) || 0}>
-        <HealthCard />
-      </Col>
-
-      <Col id="patient-timeline" xs={24} md={12} order={(isTablet && 11) || 0}>
-        <PatientResultsCard />
-      </Col>
-
-      <Col id="blood-screening" xs={24} md={12} order={(isTablet && 6) || 0}>
-        <BloodScreeningCard />
-      </Col>
-
-      <Col id="favorite-doctors" xs={24} md={24} order={(isTablet && 13) || 0}>
-        <FavoritesDoctorsCard />
-      </Col>
-
-      <Col id="covid" xs={24} md={12} order={(isTablet && 12) || 0}>
-        <CovidCard />
-      </Col>
-
-      <Col id="news" xs={24} md={24} order={(isTablet && 14) || 0}>
-        <NewsCard />
-      </Col>
+    <Row>
+      hello from mobile!
     </Row>
+  //   <Row gutter={[20, 20]}>
+  //     <StatisticsCards />
+
+  //     {isTablet && (
+  //       <Col id="map" md={24} order={4}>
+  //         <MapCard />
+  //       </Col>
+  //     )}
+
+  //     <Col id="latest-screenings" xs={24} md={12} order={(isTablet && 5) || 0}>
+  //       <ScreeningsCard />
+  //     </Col>
+
+  //     <Col id="activity" xs={24} md={12} order={(isTablet && 8) || 0}>
+  //       <ActivityCard />
+  //     </Col>
+
+  //     <Col id="treatment-plan" xs={24} md={24} order={(isTablet && 10) || 0}>
+  //       <TreatmentCard />
+  //     </Col>
+
+  //     <Col id="health" xs={24} md={12} order={(isTablet && 9) || 0}>
+  //       <HealthCard />
+  //     </Col>
+
+  //     <Col id="patient-timeline" xs={24} md={12} order={(isTablet && 11) || 0}>
+  //       <PatientResultsCard />
+  //     </Col>
+
+  //     <Col id="blood-screening" xs={24} md={12} order={(isTablet && 6) || 0}>
+  //       <BloodScreeningCard />
+  //     </Col>
+
+  //     <Col id="favorite-doctors" xs={24} md={24} order={(isTablet && 13) || 0}>
+  //       <FavoritesDoctorsCard />
+  //     </Col>
+
+  //     <Col id="covid" xs={24} md={12} order={(isTablet && 12) || 0}>
+  //       <CovidCard />
+  //     </Col>
+
+  //     <Col id="news" xs={24} md={24} order={(isTablet && 14) || 0}>
+  //       <NewsCard />
+  //     </Col>
+  //   </Row>
   );
+
+
 
   return (
     <>
       <PageTitle>{t('common.medical-dashboard')}</PageTitle>
       {isDesktop ? desktopLayout : mobileAndTabletLayout}
     </>
+
+      
+
   );
 };
 
