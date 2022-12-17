ef8f9c3ddc1d80654c6d217dcd3491dccb462e5f
Matt <mattkennedy416@gmail.com>
2022-12-17 15:56:07 -0700

    started to add modal to notebook plot

--- src/components/notebooks/NotebookPlot.tsx
+++ src/components/notebooks/NotebookPlot.tsx
@@ -4,7 +4,7 @@ import 'react-base-table/styles.css'
 
 import {Dropdown, MenuProps, Space} from 'antd';
 import { DownOutlined } from "@ant-design/icons";
-import { Button } from 'antd';
+import { Button, Select, Modal } from 'antd';
 
 import Plot from 'react-plotly.js'
 
@@ -12,8 +12,21 @@ export const NotebookPlot: React.FC = () => {
 
 
 
+
+  function handleSelectVariable(value: string) {
+    console.log(value);
+}
+
+  function showModal() {
+    console.log("showing full screen modal with large plot!")
+  }
+
     return(
         <>
+
+
+        {/* do we also need to add tabs in here so we can have multiple images per cell? */}
+            
         
         <Plot
         data={[
@@ -28,6 +41,50 @@ export const NotebookPlot: React.FC = () => {
         ]}
         layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
       />
+
+
+        <Select
+            defaultValue="select variable"
+            style={{ width: 200 }}
+            onChange={handleSelectVariable}
+            options={[
+                {
+                value: 'select variable',
+                label: 'Select Variable',
+                }
+            ]} />
+
+        <Select
+            defaultValue="plot type"
+            style={{ width: 120 }}
+            onChange={handleSelectVariable}
+            options={[
+                {
+                value: 'plot type',
+                label: 'Plot Type',
+                },
+                {
+                  value: 'scatter',
+                  label: 'Scatter',
+                },
+                {
+                  value: 'line',
+                  label: 'Line',
+                },
+                {
+                  value: 'bar',
+                  label: 'Bar',
+                },
+            ]} />
+
+      <Button type="primary" onClick={showModal}>Expand</Button>
+
+      {/* <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal> */}
+
         </>
     )
 
