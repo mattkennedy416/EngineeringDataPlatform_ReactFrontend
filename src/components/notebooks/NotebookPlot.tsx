b51ed83c0fc92fa82cfb9fe8ad69bf89cfd9d03c
Matt <mattkennedy416@gmail.com>
2022-12-20 21:20:50 -0700

    split out the plot data parameters into functions that format loaded data into the specific plot type

--- src/components/notebooks/NotebookPlot.tsx
+++ src/components/notebooks/NotebookPlot.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 
 import BaseTable, { Column } from 'react-base-table'
 import 'react-base-table/styles.css'
@@ -11,24 +12,63 @@ import Plot from 'react-plotly.js'
 export const NotebookPlot: React.FC = () => {
 
 
+  
+  const [showingModal, setShowingModal] = useState(false); // this seems to default to false? it works
+  const [inlineDiagramWidth, setInlineDiagramWidth] = useState(320);
+  const [inlineDiagramHeight, setInlineDiagramHeight] = useState(240);
+  const [modalDiagramWidth, setModalDiagramWidth] = useState(1000);
+  const [modalDiagramHeight, setModalDiagramHeight] = useState(800);
+
+  console.log(inlineDiagramWidth.toString() + " -- " + inlineDiagramHeight.toString());
+  console.log(modalDiagramWidth.toString() + " -- " + modalDiagramHeight.toString());
+
+  let allLoadedData = {'x': [1,2,3],
+                      'y': [2,6,3]};
+
+  function generateScatterData(xVar, yVar) {
+    return {x: allLoadedData[xVar],
+            y: allLoadedData[yVar],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'}}
+  }
+
+  function generateBarData(xVar, yVar) {
+    return {x: allLoadedData[xVar],
+            y: allLoadedData[yVar],
+            type: 'bar'}
+  }
+
+  let currentPlots = [generateScatterData('x','y'),
+                      generateBarData('x','y')]
+
+  // {
+  //   x: [1, 2, 3],
+  //   y: [2, 6, 3],
+  //   type: 'scatter',
+  //   mode: 'lines+markers',
+  //   marker: {color: 'red'},
+  // },
 
 
   function handleSelectVariable(value: string) {
     console.log(value);
 }
 
-  function showModal() {
-    console.log("showing full screen modal with large plot!")
+  function openModal() {
+    setShowingModal(true);
   }
 
-    return(
-        <>
+  function closeModal() {
+    setShowingModal(false);
+  }
 
+  const inlineLayout = (
+    <>
 
-        {/* do we also need to add tabs in here so we can have multiple images per cell? */}
-            
-        
-        <Plot
+    {/* do we also need to add tabs in here so we can have multiple images per cell?z */}
+
+            {/* <Plot
         data={[
           {
             x: [1, 2, 3],
@@ -39,8 +79,13 @@ export const NotebookPlot: React.FC = () => {
           },
           {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
         ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-      />
+        layout={ {width: {diagramWidth}, height: {diagramHeight}, title: 'A Fancy Plot'} }
+      /> */}
+
+        <Plot
+            data={currentPlots}
+            layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+          />
 
 
         <Select
@@ -77,13 +122,46 @@ export const NotebookPlot: React.FC = () => {
                 },
             ]} />
 
-      <Button type="primary" onClick={showModal}>Expand</Button>
+      <Button type="primary" onClick={openModal}>Expand</Button>
+    </>
+  )
 
-      {/* <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal> */}
+  const modalLayout = (
+    <>
+    {/* <p>hello world!</p>
+
+    <Button type="primary" onClick={toggleModal}>Close</Button> */}
+    
+    <Modal title="Basic Modal" 
+          style={{top:5}}
+          open={showingModal} 
+          onOk={closeModal} 
+          onCancel={closeModal}
+          width={1200}
+          footer={null}
+          >
+        
+        <Plot
+            data={currentPlots}
+            layout={ {width: 1000, height: 800, title: 'A Fancy Plot'} }
+          />
+
+        
+      </Modal>
+    
+    </>
+  )
+
+    return(
+        <>
+
+
+        
+            {showingModal ? modalLayout : inlineLayout}
+        
+
+
+
 
         </>
     )
